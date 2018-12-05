8de55a97c988eae3d28b68a0f1f784501d6805ec
srandle93 <randle.symone.l@gmail.com>
2018-12-04 20:33:57 -0600

    Updated form.

--- project3/src/components/Login/Login.js
+++ project3/src/components/Login/Login.js
@@ -6,19 +6,9 @@ function Login() {
       <div className="row">
         <form className="col s12">
           <div className="row">
-            <div className="input-field col s6">
-              <input id="first_name" type="text" className="validate"></input>
-              <label for="first_name">First Name</label>
-            </div>
-            <div className="input-field col s6">
-              <input id="last_name" type="text" className="validate"></input>
-              <label for="last_name">Last Name</label>
-            </div>
-          </div>
-          <div className="row">
             <div className="input-field col s12">
-              <input id="username" type="text" className="validate"></input>
-              <label for="username">Username</label>
+              <input id="email" type="email" className="validate"></input>
+              <label for="email">Email</label>
             </div>
           </div>
           <div className="row">
@@ -27,12 +17,7 @@ function Login() {
               <label for="password">Password</label>
             </div>
           </div>
-          <div className="row">
-            <div className="input-field col s12">
-              <input id="email" type="email" className="validate"></input>
-              <label for="email">Email</label>
-            </div>
-          </div>
+          <a class="waves-effect waves-light btn-small">Login</a> 
         </form>
       </div>
       </div>
