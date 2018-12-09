a5c4650c6bedbd277896408271ced3b0383eccd0
Thomas Winters <winterst85@gmail.com>
2018-12-09 13:56:18 -0600

    folder structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from '../Navigation';
-import LandingPage from '../Landing';
+// import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
 import SignInPage from '../SignIn';
 import PasswordForgetPage from '../PasswordForget';
