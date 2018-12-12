198d4d6601b975781a7d1b8ab3b6c5b7c6d863d4
Thomas Winters <winterst85@gmail.com>
2018-12-11 18:37:26 -0600

    deployment

--- project3/server.js
+++ project3/server.js
@@ -1,17 +1,22 @@
-//server.js
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 8080;
+const express = require("express");
+const logger = require("morgan");
+// const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+const PORT = process.env.PORT || 3001;
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBookList");
+
+app.listen(PORT, () => {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
