96aa0b7b5e7615bee4b38209a661dea388cf2132
Thomas Winters <winterst85@gmail.com>
2018-12-11 19:03:13 -0600

    deploy

--- project3/server.js
+++ project3/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const routes = require("./src/index");
 const app = express();
+const path = require('path')
 const PORT = process.env.PORT || 3001;
 
 app.use(logger("dev"));
@@ -11,6 +12,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
 }
+
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
+
 app.use(routes);
 
 app.listen(PORT, () => {
